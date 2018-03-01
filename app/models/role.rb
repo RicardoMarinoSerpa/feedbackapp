bab7313184f20c414ba35979b491112ccc10d58b
Raghu Betina <raghu@firstdraft.com>
2018-03-01 22:39:04 +0000

    Direct association between roles and subdimensions

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,9 @@
 class Role < ApplicationRecord
   # Direct associations
 
+  has_many   :subdimensions,
+             :dependent => :destroy
+
   has_many   :staffings,
              :dependent => :destroy
 
--- app/models/subdimension.rb
+++ app/models/subdimension.rb
@@ -1,6 +1,8 @@
 class Subdimension < ApplicationRecord
   # Direct associations
 
+  belongs_to :role
+
   # Indirect associations
 
   # Validations
