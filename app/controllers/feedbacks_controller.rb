1cab79235dcf15a3d9b45307bcb592186d3e07c8
Raghu Betina <raghu@firstdraft.com>
2018-03-01 22:39:45 +0000

    Only feedback user can modify feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,4 +1,14 @@
 class FeedbacksController < ApplicationController
+  before_action :current_user_must_be_feedback_sender_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_feedback_sender_user
+    feedback = Feedback.find(params[:id])
+
+    unless current_user == feedback.sender_user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @feedbacks = Feedback.all
 
